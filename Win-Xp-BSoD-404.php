992f7ade93a52464010ade9c4a10a6f1eb6add2e
Sean Hill <azusdevelopment@gmail.com>
2017-01-06 20:32:11 -0800

    Create Win-Xp-BSoD-404.php

---
+++ Win-Xp-BSoD-404.php
@@ -0,0 +1,53 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>404 - SysEr/PgNtFnd</title>
+		<meta name="description" content="System Error Code 404 - Page Not Found">
+		<meta name="author" content="Azus">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<style type=text/css>
+		body {
+		background-color: #000090;
+		font-family: "Lucida Console", Monaco, monospace;
+		font-weight: bold;
+		color: #FFFFFF;
+		}
+		h1 {
+		font-weight: 900;
+		}
+		</style>
+	</head>
+	<body>
+		<p>
+			<br>
+			<br>
+			<center>
+				<h1> 404 - SysEr/PgNtFnd
+			</center>
+			<br>
+			<br>
+			A problem has been detected and the forum has shut down to prevent damage to our website.
+			<br>
+			<br>
+			<br>
+			If this is the first time you've seen this ERROR screen, go back to the page you came from. If this screen appears again, your IP address will be logged.
+			<br>
+			<br>
+			<br>
+			<br>
+			Check to be sure the URL you are trying to visit is correct. If a page is indentified in the ERROR message, forget the page ever existed because there is no content at the specified location. Try a correct URL.
+			<br>
+			<br>
+			Check with a site administrator about any site updates. Make note of the URL at the ERROR message and report it to an administrator for further information about the ERROR message. If the URL you're trying to visit is deemed, broken an administrator will be fixing it as soon as possible.
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			Technical Information:
+			<br>
+			<br>
+			*** ERROR: 0x00000404 (Page Not Found, System Damage Prevention)
+		</p>	
+	</body>
+</html>
